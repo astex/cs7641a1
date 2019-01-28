17340f03299e2135be9652d0cd21e21669217311
astex <0astex@gmail.com>
2019-01-27 22:20:36 -0500

    Fix nyt data fetch.

--- data/get.sh
+++ data/get.sh
@@ -6,8 +6,8 @@
 # NYT
 #
 # Sleep for a minute between each fetch to get around rate limiting.
-./data/nyt/get.sh 2018; sleep 1m;
-./data/nyt/get.sh 2017; sleep 1m;
-./data/nyt/get.sh 2016; sleep 1m;
-./data/nyt/get.sh 2015; sleep 1m;
-./data/nyt/get.sh 2014; sleep 1m;
+./data/nyt/get.sh 2018; sleep 60;
+./data/nyt/get.sh 2017; sleep 60;
+./data/nyt/get.sh 2016; sleep 60;
+./data/nyt/get.sh 2015; sleep 60;
+./data/nyt/get.sh 2014; sleep 60;
