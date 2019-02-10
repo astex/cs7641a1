3b0caf8307e25f67df5c9909db17f5aec6e713cf
astex <0astex@gmail.com>
2019-02-10 16:44:23 -0500

    Add a max size to IMDB SVMs.

--- src/imdb/svm.py
+++ src/imdb/svm.py
@@ -5,13 +5,14 @@ from .. import data as datalib
 
 
 SIZE_STEP = .01
+SIZE_MAX = .3
 
 
 def plot_size(data, plotter):
     ldata, tdata = datalib.partition(data, (.8, .2))
 
     size = SIZE_STEP
-    while size < 1.01:
+    while size < SIZE_MAX:
         data, = datalib.partition(ldata, (size,))
         classifier = sklearn.svm.NuSVC(gamma="scale")
         plotter.plot(classifier, size, data, tdata)
