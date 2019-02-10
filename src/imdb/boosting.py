eca463297d4f0be0917ae83b7765d5a718b1845c
astex <0astex@gmail.com>
2019-02-10 08:42:18 -0500

    Fix boosting rate plot bug.

--- src/imdb/boosting.py
+++ src/imdb/boosting.py
@@ -31,14 +31,14 @@ def plot_est(data, plotter):
         plotter.plot(classifier, est, data, tdata)
 
 
-def plot_rate(data):
+def plot_rate(data, plotter):
     ldata, tdata = datalib.partition(data, (.8, .2))
 
     rate = RATE_STEP
     while rate < RATE_MAX:
         classifier = sklearn.ensemble.GradientBoostingClassifier(
             learning_rate=rate)
-        plotter.plot(classifier, size, data, tdata)
+        plotter.plot(classifier, rate, data, tdata)
         rate += RATE_STEP
 
 
