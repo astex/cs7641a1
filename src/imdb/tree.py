0a6721855269e108128d55d80fc200e21a980ca6
astex <0astex@gmail.com>
2019-02-10 17:34:48 -0500

    Add alpha pruning plots.

--- src/imdb/tree.py
+++ src/imdb/tree.py
@@ -7,6 +7,9 @@ from .. import data as datalib
 MAX_DEPTH = 25
 SIZE_INTERVAL = .01
 
+ALPHA_STEP = .001
+ALPHA_MAX = .02
+
 
 def plot_depth(data, plotter):
     ldata, tdata = datalib.partition(data, (.8, .2))
@@ -26,6 +29,19 @@ def plot_size(data, plotter):
         size += SIZE_INTERVAL
 
 
+def plot_ccp_alpha_prune(data, plotter):
+    ldata, tdata = datalib.partition(data, (.8, .2))
+
+    alpha = ALPHA_STEP
+    while alpha < ALPHA_MAX:
+        classifier = sklearn.tree.DecisionTreeClassifier(
+            max_depth=10,
+            ccp_alpha=alpha)
+        plotter.plot(classifier, alpha, ldata, tdata)
+        alpha += ALPHA_STEP
+
+
 def register():
     datalib.register_plotfunc("tree", "depth", plot_depth)
     datalib.register_plotfunc("tree", "size", plot_size)
+    datalib.register_plotfunc("tree_prune", "alpha", plot_ccp_alpha_prune)
